5bf584fd6185c09311b0844b976c822d464131de
Christian <howuselessislife@gmail.com>
2025-06-24 21:36:30 +0800

    Remove lines in arrow|carousel

---
+++ Javascript/data.js
@@ -0,0 +1,38 @@
+export const locations = [
+  {
+    image: {
+      src: "./Images/locations/amihan.png",
+      alt: "amihan outpost picture"
+    },
+  },
+  {
+    image: {
+      src: "./Images/locations/caltex.png",
+      alt: "caltex outpost picture"
+    },
+  },
+  {
+    image: {
+      src: "./Images/locations/dictado.png",
+      alt: "dictado outpost picture"
+    },
+  },
+  {
+    image: {
+      src: "./Images/locations/rosada.png",
+      alt: "rosada outpost picture"
+    },
+  },
+  {
+    image: {
+      src: "./Images/locations/tionco.png",
+      alt: "tionco outpost picture"
+    },
+  },
+  {
+    image: {
+      src: "./Images/locations/zavalla.png",
+      alt: "zavalla outpost picture"
+    },
+  },
+]
